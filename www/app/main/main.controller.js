a234b407b07e19a4d4eb1ca70a47a8b2a94422b2
dnrodriguesm <dnrodriguesm@gmail.com>
2016-12-04 01:53:34 -0300

    Adjusting timeline for app

--- www/app/main/main.controller.js
+++ www/app/main/main.controller.js
@@ -4,4 +4,29 @@ angular
 
 function MainController($log) {
   var vm = this;
+
+  vm.countLikes = function(){
+     console.log('Like');
+  }
+
+  vm.timeline = [
+    {
+      "name": "Ágata Pereira",
+      "avatar": "../img/in-thumb-1.jpg",
+      "post": {
+        "pic": "../img/in-post-1.jpg",
+        "date": "04 Dezembro 2016",
+        "description": "Minha xota 11/10"
+      }
+    },
+    {
+      "name": "Suelen Pereira",
+      "avatar": "../img/in-thumb-2.jpg",
+      "post": {
+        "pic": "../img/in-post-2.jpg",
+        "date": "04 Dezembro 2016",
+        "description": "Minha xota 10/10"
+      }
+    }
+  ]
 }
